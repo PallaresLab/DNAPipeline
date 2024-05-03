f34ced3532dc045615a86eb5135388f1f4a0ff68
Ahuiting <104276232+Ahuiting@users.noreply.github.com>
2024-05-03 18:21:28 +0200

    Add files via upload

--- get_RG.sh
+++ get_RG.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-header=$(zcat $1 | head -n 1 )
+fq=$1
+header=$(zcat $fq | head -n 1 )
+name=${fq%%.*}
 
 IFS=':' read -ra fields <<< "$header"
 
@@ -12,5 +13,5 @@ tile=${fields[4]}
 
 read_group_id="${flowcell}.${lane}"
 
-read_group="@RG\\tID:${read_group_id}\\tSM:${instrument}\\tLB:${instrument}\\tPL:Illumina"
+read_group="@RG\\tID:${read_group_id}\\tSM:${name}\\tLB:${instrument}\\tPL:Illumina"
 echo "$read_group"
